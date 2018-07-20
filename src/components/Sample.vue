8b47cbfb7797d479ab48dfc5fe60848e3ab04db2
MartinP <martin.prinz@maibornwolff.de>
2018-07-20 17:34:18 +0200

    styling

--- src/components/Sample.vue
+++ src/components/Sample.vue
@@ -1,92 +1,153 @@
 <template>
- <div>
+ <div class="sample">
   <h1>{{title}}</h1>
-  <svg height="5000" width="1200"></svg>
+  <svg></svg>
  </div>
 </template>
 
 <script>
 import * as d3 from 'd3';
-import {datamuc} from './datamuc.js';
+import { datamuc } from './datamuc.js';
 
 export default {
     name: 'Sample',
     props: {
         title: {
             type: String,
-            required: true,
-        },
+            required: true
+        }
     },
     data() {
         return {
-           _list: [1.1, 2.2, 3.4, 4.1, 8.5, 9.9, 7.1, 8.8, 9.4],
+            _list: [1.1, 2.2, 3.4, 4.1, 8.5, 9.9, 7.1, 8.8, 9.4],
             list: datamuc
         };
     },
 
     mounted() {
-        const max = Math.max(...this.list.map(it => it.hauptwohnsitzbevölkerung)) + 2;
-        const factor = 900 / max;
-        this.list.forEach((item, i) =>  {
-            console.log(factor * item.hauptwohnsitzbevölkerung);
-            const group = d3.select('svg')
-            .append('g').attr('id', 'group_' + item.bezirksteil);
+        const height = 3000;
+        const width = 2000;
 
-            group.append('rect')
-            .attr('x', 0)
-            .attr('y', i * 25)
+        const max = d3.max(datamuc, d => d['hauptwohnsitzbevölkerung']);
+        const svg = d3
+            .select('svg')
+            .attr('height', height)
+            .attr('width', width);
+
+        const scaleFactor = 1300 / max;
+
+        svg
+            .selectAll(null)
+            .data(datamuc)
+            .enter()
+            .append('g');
+
+        svg
+            .selectAll('g')
+            .append('rect')
+            .attr('x', -20)
+            .attr('y', function(d, i) {
+                return i * 25;
+            })
             .attr('rx', 10)
             .attr('ry', 10)
             .attr('width', 0)
             .attr('height', 20)
-            .style('fill', d3.rgb(233, 180, i * 3))
+            .style('opacity', 0.6)
+            .style('fill', function(d, i) {
+                return d3.rgb(i * 10, 90 + i * 2, 150);
+            })
+            .on('mouseover', function(d, i) {
+                d3
+                    .selectAll('rect')
+                    .transition()
+                    .duration(300)
+                    .style('opacity', 0.6);
+
+                d3
+                    .select(this)
+                    .transition()
+                    .duration(300)
+                    .style('opacity', 1);
+
+                const posx = +d3.select(this).attr('width') + 10;
+
+                d3
+                    .selectAll('text')
+                    .filter(function(k, j) {
+                        return j === i;
+                    })
+                    .transition()
+                    .duration(500)
+                    .delay(200)
+                    .attr('x', posx)
+                    .text(function(d, i) {
+                        return `${d.bezirksteil_bezeichnung.substr(
+                            5
+                        )} (${d.hauptwohnsitzbevölkerung} Hauptwohnsitze)`;
+                    })
+                    .style('opacity', 1);
+            })
+
+            .on('mouseout', function(d, i) {
+                let posx = +d3.select(this).attr('width');
+
+                d3
+                    .selectAll('text')
+                    .filter(function(k, j) {
+                        return j === i;
+                    })
+                    .transition()
+                    .duration(500)
+                    .attr('x', posx)
+                    .text(function(d, i) {
+                        return `${d.bezirksteil_bezeichnung.substr(5)}`;
+                    })
+                    .style('opacity', 0.2);
+            })
+
             .transition()
-            //.delay((max - item.bezirksteil) * 50)
             .duration(1000)
-            .attr("width", factor * item.hauptwohnsitzbevölkerung)
+            .attr('width', function(d) {
+                return 100 + scaleFactor * d['hauptwohnsitzbevölkerung'];
+            });
 
-            group.append('rect')
-            .attr('x', factor * item.hauptwohnsitzbevölkerung)
-            .attr('y', 10 + i * 25)
-            
-            .attr('width', 0)
-            .attr('height', 1)
-            .style('fill', 'black')
-            .style('opacity', .2)
-            .transition()
-            .duration(500)
-            .delay(1000)
-            .attr("width", 10)
-
-            group.append('rect')
-            .attr('x', 0)
-            .attr('y', i * 25)
-            .attr('rx', 10)
-            .attr('ry', 10)
-            .attr('width', 1200)
-            .attr('height', 20)
-            .style('opacity', .2)
-            .style('fill', d3.rgb(233, 180, i * 30))
-
-            group.append('text')
-            .attr('x', 30 + factor * item.hauptwohnsitzbevölkerung)
-            .attr('y', 15 + i * 25)
-            .text(`${item.bezirksteil_bezeichnung.substr(5)}: ${item.hauptwohnsitzbevölkerung}`)
+        svg
+            .selectAll('g')
+            .append('text')
+            .attr('y', function(d, i) {
+                return 15 + i * 25;
+            })
             .style('opacity', 0)
+            .attr('x', function(d) {
+                return 125 + scaleFactor * d['hauptwohnsitzbevölkerung'];
+            })
             .transition()
+            .duration(300)
             .delay(1000)
-            .duration(500)
-            .attr('x', 20 + factor * item.hauptwohnsitzbevölkerung)
-            .style('opacity', 0.5)
-        });
-        
-    },
+            .attr('x', function(d) {
+                return 100 + scaleFactor * d['hauptwohnsitzbevölkerung'];
+            })
+            .style('opacity', 0.2)
+            .style('fill', 'white')
+            .text(function(d, i) {
+                return `${d.bezirksteil_bezeichnung.substr(5)}`;
+            });
+    }
 };
 </script>
 
-<style lang="sass">
-svg
-    margin: 25px
-
+<style lang="scss">
+.sample {
+    // background: linear-gradient(to bottom, rgba(233, 180, 0, 0.3), rgba(233, 180, 255, 0.4));
+    background-color: rgb(31, 31, 31);
+    padding-top: 100px;
+    
+    h1{
+        margin-bottom:100px;
+        opacity: .8;
+        color: white;
+    }
+}
 
 </style>
